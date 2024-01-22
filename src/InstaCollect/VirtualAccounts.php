e28e3663524aba4e35249182f7038ad28a07bb2c
Athul Raj <mail@athulraj.com>
2024-01-22 07:56:04 +0530

    fix

--- src/InstaCollect/VirtualAccounts.php
+++ src/InstaCollect/VirtualAccounts.php
@@ -2,11 +2,9 @@
 
 namespace ScaleXY\Easebuzz\InstaCollect;
 
-use Exception;
-use Illuminate\Support\Facades\Http;
 use ScaleXY\Easebuzz\EasebuzzServiceHandler;
 
-class VirtualAcounts
+class VirtualAccounts
 {
 	const base_path = "https://wire.easebuzz.in";
 	// const base_path = "https://stoplight.io/mocks/easebuzz/neobanking/90373567";
@@ -30,16 +28,17 @@ class VirtualAcounts
 			"label"
 		])->data->virtual_account;
 	}
-	public function handleWebhook($virtual_account_number, $virtual_payment_address = null, $label = null, $description = null): void
-	{
-		// $path = "/api/v1/insta-collect/virtual_accounts/";
-		// return $this->client->makePOSTRequestType01(self::base_path . $path, [
-		// 	"virtual_account_number" => $virtual_account_number,
-		// 	"virtual_payment_address" => $virtual_payment_address ?? $virtual_account_number,
-		// 	"label" => $label ?? "VA " . $virtual_account_number,
-		// 	"description" => $description ?? "No description",
-		// ], [
-		// 	"label"
-		// ]);
-	}
+	
+	// public function handleWebhook($virtual_account_number, $virtual_payment_address = null, $label = null, $description = null): void
+	// {
+	// 	$path = "/api/v1/insta-collect/virtual_accounts/";
+	// 	return $this->client->makePOSTRequestType01(self::base_path . $path, [
+	// 		"virtual_account_number" => $virtual_account_number,
+	// 		"virtual_payment_address" => $virtual_payment_address ?? $virtual_account_number,
+	// 		"label" => $label ?? "VA " . $virtual_account_number,
+	// 		"description" => $description ?? "No description",
+	// 	], [
+	// 		"label"
+	// 	]);
+	// }
 }
